f3d9f990c567265ecac135285aa8d530d38d7314
mauimatihiko <maori.programers@slmail.me>
2022-08-08 23:37:48 +0100

    installed tailwindcss configurations

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="bg-gray-900 text-white w-full h-screen">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <code>Maaori</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
